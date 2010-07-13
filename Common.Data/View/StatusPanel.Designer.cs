5aa552cec792efc8d478b08b97b241bac6f20b2e
Thomas Siebers <tom@tsiebers.de>
2010-07-13 17:53:52 +0200

    Added the StatusPanel user control.

---
+++ Common.Data/View/StatusPanel.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Common.Data.View {
+    partial class StatusPanel {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StatusPanel));
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.WaitingPicture = new System.Windows.Forms.PictureBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.WaitingPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblProgress.BackColor = System.Drawing.Color.Transparent;
+            this.lblProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgress.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.lblProgress.Location = new System.Drawing.Point(126, 9);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(314, 246);
+            this.lblProgress.TabIndex = 5;
+            this.lblProgress.Text = "Status";
+            this.lblProgress.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // WaitingPicture
+            // 
+            this.WaitingPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.WaitingPicture.BackColor = System.Drawing.Color.Transparent;
+            this.WaitingPicture.Image = ((System.Drawing.Image)(resources.GetObject("WaitingPicture.Image")));
+            this.WaitingPicture.InitialImage = null;
+            this.WaitingPicture.Location = new System.Drawing.Point(98, 9);
+            this.WaitingPicture.Name = "WaitingPicture";
+            this.WaitingPicture.Size = new System.Drawing.Size(30, 248);
+            this.WaitingPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.WaitingPicture.TabIndex = 4;
+            this.WaitingPicture.TabStop = false;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.Control;
+            this.btnCancel.Location = new System.Drawing.Point(379, 241);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Abbrechen";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // StatusPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.WaitingPicture);
+            this.Name = "StatusPanel";
+            this.Size = new System.Drawing.Size(457, 267);
+            ((System.ComponentModel.ISupportInitialize)(this.WaitingPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.PictureBox WaitingPicture;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Common.Data/View/StatusPanel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Common.Data.View {
+    public partial class StatusPanel : UserControl {
+
+        #region Properties
+
+        [DefaultValue("Status")]
+        public String Message {
+            get {
+                return lblProgress.Text;
+            }
+
+            set {
+                lblProgress.Text = value;
+            }
+        }
+
+        [DefaultValue(false)]
+        public Boolean SupportsCancellation {
+            get {
+                return btnCancel.Visible;
+            }
+
+            set {
+                btnCancel.Visible = value;
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler Cancel;
+        protected void RaiseCancel() {
+            if (Cancel == null)
+                return;
+
+            Cancel(this, new EventArgs());
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public StatusPanel() {
+            InitializeDefaultValues();
+            InitializeComponent();
+        }
+
+        protected void InitializeDefaultValues() {
+            lblProgress.Text = "Status";
+            btnCancel.Hide();
+        }
+
+        #endregion
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            RaiseCancel();
+        }
+
+    }
+}
