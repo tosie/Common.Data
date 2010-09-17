d0417f96b9269195b802ec75697024c2ea6a1836
Thomas Siebers <tom@tsiebers.de>
2010-09-17 15:13:07 +0200

    Removed the "Push" configuration. Using "Release" instead. Additionally the build command merges all files together after a successful build and if ILMerge is available.

--- build.cmd
+++ build.cmd
@@ -2,22 +2,54 @@
 
 set framework_version=3.5
 set msb=%windir%\Microsoft.NET\Framework\v%framework_version%\MSBuild.exe
+
 set project=Common.Data\Common.Data.csproj
-set outdir=Common.Data\obj\Push
 set basename=Common.Data
-set releasedir=Release
+set outdir=Release
+
+set ilmerge="%programfiles%\Microsoft\ILMerge\ILMerge.exe"
+set ilmerge_params=/wildcards /ndebug /lib:%outdir% /target:library
+set package=Common.Data.dll
+set assemblies=Common.Data.dll Common.Configuration.dll SubSonic.Core.dll
 
 if not exist %msb% goto msbuild_not_found
 goto do_build
 
 :do_build
 echo Building the project ...
-if not exist %outdir% mkdir %outdir%
-%msb% /target:Build /property:Configuration=Push /property:OutDir=..\%releasedir%\ "%project%"
+%msb% /target:Build /property:Configuration=Release /property:OutDir=..\%outdir%\ "%project%"
 if errorlevel 1 goto build_error
 goto build_ok
 
 :build_ok
+goto merge_assemblies
+
+:merge_assemblies
+if not exist %ilmerge% goto ilmerge_not_found
+
+echo.
+echo Merging assemblies ...
+echo   Assemblies = %assemblies%
+%ilmerge% %ilmerge_params% /out:%outdir%\merged.dll %assemblies%
+if errorlevel 1 goto ilmerge_error
+goto ilmerge_ok
+
+:ilmerge_ok
+echo.
+del /q %outdir%\Common.*
+del /q %outdir%\SubSonic.*
+ren %outdir%\merged.dll %package%
+echo The assemblies have been merged into "%outdir%\%package%".
+goto end
+
+:ilmerge_error
+goto end
+
+:ilmerge_not_found
+echo.
+echo ILMerge not found (expected it at %ilmerge%).
+echo Assemblies will not be merged.
+echo You can download it from http://research.microsoft.com/en-us/people/mbarnett/ilmerge.aspx
 goto end
 
 :msbuild_not_found
