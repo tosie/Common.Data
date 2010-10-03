85dc92cf1fc39948994a14d90fa7e65787696ced
Thomas Siebers <tom@tsiebers.de>
2010-10-03 10:17:02 +0200

    Added a little inline documentation to the FasterListView.

--- Common.Data/ViewHelpers/FasterListView.cs
+++ Common.Data/ViewHelpers/FasterListView.cs
@@ -6,7 +6,9 @@ using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Common.Data {
-    // See: http://www.virtualdub.org/blog/pivot/entry.php?id=273
+    /// <summary>
+    /// Is a child class of the original <see cref="System.Windows.Forms.ListView"/>, that improves the drawing speed. See http://www.virtualdub.org/blog/pivot/entry.php?id=273 for details.
+    /// </summary>
     public class FasterListView : ListView {
         
         [DllImport("user32.dll")]
@@ -23,10 +25,17 @@ namespace Common.Data {
 
         private const uint NM_CUSTOMDRAW = unchecked((uint)-12);
 
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public FasterListView() : base() {
             SendMessage(Handle, LVM_SETTEXTBKCOLOR, IntPtr.Zero, unchecked((IntPtr)(int)0xFFFFFF));
         }
 
+        /// <summary>
+        /// Overwritten window handler, that intercepts the NM_CUSTOMDRAW message and ignores it.
+        /// </summary>
+        /// <param name="m"></param>
         protected override void WndProc(ref Message m) {
             if (m.Msg == 0x204E) {
                 NMHDR hdr = (NMHDR)m.GetLParam(typeof(NMHDR));
